3df4c1e520718c9145c2dc0a8507f0f50249f401
Anna Konopka <aniakon207@gmail.com>
2025-10-27 13:46:58 +0100

    zmieniłam angielski na polski

--- 02-TypesAndVariables/1.3.py
+++ 02-TypesAndVariables/1.3.py
@@ -1,13 +1,13 @@
 ###
 # BMI Calculator
 #
-height = input('Enter your height in cm: ')
-weight = input('Enter your weight in kg: ')
+height = input('Wpisz wzrost w cm: ')
+weight = input('Wpisz wagę w kg: ')
 height = int(height)
 weight = int(weight)
 bmi = weight / (height/100)**2
 bmi = round(bmi,2)
-print('Your BMI is', bmi)
-if bmi<=18.5:print('masz niedowagę')
-if bmi>18.5 and bmi<25:print('masz wagę prawidłową')
-if bmi>=25:print('masz nadwagę')
+print('Twoje BMI to:', bmi)
+if bmi<=18.5:print('Masz niedowagę')
+if bmi>18.5 and bmi<25:print('Masz wagę prawidłową')
+if bmi>=25:print('Masz nadwagę')
